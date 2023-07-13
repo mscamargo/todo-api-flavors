3cef55c2d6065ce04f0ea38a529832758baf875d
mscamargo <mscamargo.dev@gmail.com>
2023-07-12 21:49:03 -0300

    add first complete test

--- e2e/todo.test.js
+++ e2e/todo.test.js
@@ -1,26 +1,55 @@
-import assert from 'node:assert/strict'
+import assert from 'node:assert'
 import { describe, it, before, after } from 'node:test'
+
 import { once } from 'node:events'
 
-const PORT = 3000
+const URL = 'http://localhost:3000/todo'
+
+async function setup () {
+  const server = (await import('../nodejs-vanilla/src/main.js')).server
+  await once(server, 'listening')
+  return server
+}
+
+/**
+ * @param {import 'http'.Server} server
+ */
+async function teardown (server) {
+  await new Promise(resolve => server.close(resolve))
+}
 
 describe('[POST] /todo', () => {
+  /**
+   * @type {import 'http'.Server}
+   */
   let server
+
   before(async () => {
-    server = (await import('../nodejs-vanilla/src/main.js')).server
-    await once(server, 'listening')
-  })
-  after((done) => {
-    server.close(done)
+    server = await setup()
   })
 
-  it('should create a to-do item correctly', async () => {
-    const response = await fetch(`http://localhost:${PORT}/todo`, {
-      method: 'POST'
+  after(async () => await teardown(server))
+
+  describe('when no authorization is provided', () => {
+    /**
+     * @type {Response}
+     */
+    let response
+    before(async () => {
+      response = await fetch(URL, {
+        method: 'POST'
+      })
     })
-    assert.equal(response.status, 201)
+    it('should return the status code 401', () => {
+      assert.equal(response.status, 401)
+      assert.equal(response.statusText, 'Unauthorized')
+    })
+    it('should return an empty body', async () => {
+      assert.equal(response.headers.get('Content-Type'), null)
+      assert.equal(response.headers.get('Content-Length'), null)
+      assert.equal(await response.text(), '')
+    })
   })
-  it.todo('should return a validation error for an invalid a to-do item')
 })
 
 describe('[GET] /todo', () => {
--- nodejs-vanilla/src/main.js
+++ nodejs-vanilla/src/main.js
@@ -1,5 +1,12 @@
+import { once } from 'node:events'
 import http from 'node:http'
 
-export const server = http.createServer((request, response) => {
-  response.writeHead(201).end()
-}).listen(3000)
+export const server = http.createServer(async (request, response) => {
+  if (request.headers['content-length'] !== '0') {
+    const body = await once(request, 'data')
+    console.log({ body })
+  }
+  response.writeHead(401)
+  response.end()
+})
+  .listen(3000)
